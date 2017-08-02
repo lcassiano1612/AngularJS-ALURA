e985b31dbe65ed0544af2462cb010a985d22f253
Lucas Oliveira <Lucas Oliveira>
2017-08-02 00:20:50 -0300

    foi adicionado cadastro das fotos e validação do formulario

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,17 +1,25 @@
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function($scope, $http){
 
     $scope.foto = {};
+    $scope.mensagem = '';
 
     $scope.submeter = function(){
 
-        $http.post('v1/fotos', $scope.foto).success(function(){
-            
+        if($scope.formulario.$valid){
 
-        }).error(function(res){
+            $http.post('v1/fotos', $scope.foto).success(function(){
+                
+                $scope.mensagem = "Foto cadastrado com successo!";
+                $scope.foto = {};
 
-            console.log(res)
+            }).error(function(res){
 
-        });
+                $scope.mensagem = "Não foi possivel cadastrar foto!";
+                console.log(res);
+
+            });
+
+        }
 
     }
 
